96a4a1f78380f19dba073375ecb0ae6d83759577
einwenigtot <colindillier@icloud.com>
2019-06-27 15:31:44 +0200

    helper klasse hinzugefügt

---
+++ ExtremeBookHolding/Src/Core/JournalHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeBookHolding.Core
+{
+    public static class JournalHelper
+    {
+        public static void AddBuchungssatz(this ObservableCollection<Journal> journals, decimal value , Account account, string text ,bool addToCredit)
+        {
+            var journal = journals.FirstOrDefault(x => x.Account == account);
+            if(journal != null)
+            {
+                if(addToCredit)
+                {
+                    journal.CreditAccountingRecords.Add(new AccountingRecord() { Account = account, Text = text, Value = value });
+                }
+                else
+                {
+                    journal.DebitAccountingRecords.Add(new AccountingRecord() { Account = account, Text = text, Value = value });
+                }
+            }
+            journals.Add()
+        }
+    }
+}
